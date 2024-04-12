dd41c82643dd22cfe5f8d7637606de9b2dede39d
Leonardo Hernández <leonardojhr03@gmail.com>
2024-04-12 01:39:52 -0400

    Eliminar el botón de crear y crear otro registro

--- app/Filament/Resources/SaleResource/Pages/CreateSale.php
+++ app/Filament/Resources/SaleResource/Pages/CreateSale.php
@@ -9,4 +9,5 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateSale extends CreateRecord
 {
     protected static string $resource = SaleResource::class;
+    protected static bool $canCreateAnother = false;
 }
